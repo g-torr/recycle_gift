34740ec15f30e369056bcc6f3c0c38e7ed7b0987
gtorrisi <giuseppe.torrisi@ens-lyon.fr>
2018-03-17 17:51:12 +0000

    1st commit

---
+++ create_social_interactions.m
@@ -0,0 +1,52 @@
+function neighbours = create_social_interactions(n_friends)
+% This funtion creates the social network of who knows who.
+%   Considering the total number of friends as n_friends, each of them can have n_neighbours friends. Who are the new friends is stored in a cell of n_friends  
+n_neighbours=ceil(n_friends*rand(n_friends,1))% each node can have any number of neighbours,from an uniform distributon of integers till n_friends
+neighbours=cell(n_friends,1);
+for i=1:n_friends
+    neighbours{i} = [randperm(n_friends,n_neighbours(i)),i]; %take randomly n_neighbours as friends and add itself
+end
+save('neighbours.mat','neighbours');
+%plot the grah
+F=zeros(n_friends);
+for i=1:n_friends
+    F(i,neighbours{i})=1;% this is the network of social frienship
+end
+NF=~F;% network of who does not know who
+F=F-eye(n_friends);
+
+G=digraph(F);
+p=plot(G,'Layout','circle');
+p.NodeLabel = {};
+p.ArrowSize=22;
+p.LineWidth=1.5;
+% Custom labels
+hold on
+names=string(1:n_friends);
+for i=1:length(names)
+    text(p.XData(i), p.YData(i), names(i), 'FontSize', 18);
+end
+ax=gca();
+ax.Visible= 'off';
+tightfig()
+saveas(gcf,'friendship.png')
+hold off
+
+G2=digraph(NF);
+p2=plot(G2,'Layout','circle');
+p2.NodeLabel = {};
+p2.ArrowSize=22;
+p2.LineWidth=1.5;
+% Custom labels
+hold on
+names=string(1:n_friends);
+for i=1:length(names)
+    text(p2.XData(i), p2.YData(i), names(i), 'FontSize', 18);
+end
+ax=gca();
+ax.Visible= 'off';
+tightfig()
+print(gcf,'non-friendship','-dpng')
+end
+
+
---
+++ make_presents.m
@@ -0,0 +1,30 @@
+function [cost,gift_owned,gifted] =make_presents(state)
+    global neighbours
+    state=round(state);
+    % state is a vectors which contains n_friends+ (n_friends-1) variables
+    n_friends=(length(state)+1)/2;
+    x=state(1:n_friends); %these are the first n_friends, and they represents the people I visit
+    if (length(unique(x))<n_friends)% we want to go to all friends, so I penalise heavily if not do so
+        cost=n_friends^2;
+        return;
+    end
+    odds=state(n_friends:end);% this variable says, when I can choose among different presents  (i.e. in the else below) which present I gift. It has not effect in the ''if'' case below. But in general I cannot predict a priori if it is needed or not.
+    odds=odds/n_friends; % I want it normalised between [0,1]
+    cost=1; % in the first step I need to buy a presents, 
+    gift_owned=x(1);% I receive the gift from the first friend
+    gifted=zeros(n_friends,1);% this is just for record of which  gifth are given to friends, it is not used in the evalutation of the cost. 0 means that the gifth is buyed, otherwise it says from which person the gift comes from 
+    for i=2:n_friends
+        indeces=find(~ismember(gift_owned,neighbours{x(i)})); % gives the indeces of the presents that can be gifted
+        if  isempty(indeces) % given the presents I have, all of them  would be spotted by the receiver
+            cost=cost+1;
+            gift_owned(end +1)=x(i);
+        else
+            %index of the selected gifth is:
+            indx=indeces(ceil(odds(i-1)*length(indeces)));% I pick one gift among the list of possibles( i.e. that do not clash with social relations). To do so I use pick one of the indices according to the number odds.  
+            gifted(i)=gift_owned(indx);% this is the gift I  make to x(i)
+            gift_owned(indx)=x(i); % I receive the gift
+        end
+    %gift_owned
+    %gifted    
+    end
+end
